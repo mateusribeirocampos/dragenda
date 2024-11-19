112b5d407b0e2df2d0fa9f77b3f443eeb2a61655
mateusribeirocampos <mateusrcampos@gmail.com>
2024-11-19 18:06:01 -0300

    feat: database integrated with sqlite.js

--- agendei-api/src/repositories/repository.doctor.js
+++ agendei-api/src/repositories/repository.doctor.js
@@ -1,22 +1,18 @@
+// Importa a função query do módulo sqlite.js localizado no diretório database
+import { query } from "../database/sqlite.js";
 
-
+// Define uma função assíncrona chamada Listar
 async function Listar() {
 
-  // simula o acesso ao banco
-  const doctors = [
-    { id: 1, name: "Mateus", specialty: "Cardiologista", icon: "M" },
-    { id: 2, name: "João", specialty: "Ortopedista", icon: "M" },
-    { id: 3, name: "Maria", specialty: "Dermatologista", icon: "F" },
-    { id: 4, name: "José", specialty: "Oftalmologista", icon: "M" },
-    { id: 5, name: "Ana", specialty: "Pediatra", icon: "F" },
-    { id: 6, name: "Carlos", specialty: "Ginecologista", icon: "M" },
-    { id: 7, name: "Juliana", specialty: "Nutricionista", icon: "F" },
-    { id: 8, name: "Mariana", specialty: "Fisioterapeuta", icon: "F" },
-    { id: 9, name: "Rafael", specialty: "Psicólogo", icon: "M" },
-    { id: 10, name: "Lucas", specialty: "Psiquiatra", icon: "M" },
-  ];
+  // Define a consulta SQL para selecionar todos os registros da tabela doctors, ordenados pelo nome
+  let sql = "select * from doctors order by name";
 
+  // Executa a consulta SQL usando a função query e armazena o resultado na constante doctors
+  const doctors = await query(sql, []);
+
+  // Retorna o resultado da consulta, que é uma lista de médicos
   return doctors;
 }
 
+// Exporta a função Listar como parte do objeto padrão exportado por este módulo
 export default { Listar };
