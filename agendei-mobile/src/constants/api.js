8781022d54317aa424c60c96114f2095d5b1d1b4
mateusribeirocampos <mateusrcampos@gmail.com>
2024-12-02 15:34:39 -0300

    feat: API_KEY and API_URL insert in .env

--- agendei-mobile/src/constants/api.js
+++ agendei-mobile/src/constants/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
 const api = axios.create({
-  baseURL: "http://192.168.3.7:3001"
+  baseURL: "API_URL",
 });
 
 export default api;
