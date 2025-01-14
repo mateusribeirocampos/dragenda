0cd16474f7722ffc9ae1ede11a9731a5de610258
mateusribeirocampos <mateusrcampos@gmail.com>
2025-01-14 17:29:22 -0300

    search and filter to date added and logout done

--- agendei-web/src/components/navbar/navbar.jsx
+++ agendei-web/src/components/navbar/navbar.jsx
@@ -1,17 +1,21 @@
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo-white.png";
-import { useEffect, useState } from "react";
+import api from "../../constants/api";
 
 function Navbar() {
-  const [userName, setUserName] = useState("");
 
-  useEffect(() => {
-    const name = localStorage.getItem("sessionName");
-    if(name) {
-      setUserName(name);
-    }
-  }, []);
+  const navigate = useNavigate();
+
+  function Logout() {
+    console.log("Logout...");
+    localStorage.removeItem("sessionToken");
+    localStorage.removeItem("sessionId");
+    localStorage.removeItem("sessionEmail");
+    localStorage.removeItem("sessionName");
+    api.defaults.headers.common['Authorization'] = "";
+    navigate("/");
+  }
 
   return (
     <nav
@@ -54,12 +58,12 @@ function Navbar() {
             <li className="nav-item">
               <div className="btn-group">
                 <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                  {userName}
+                  {localStorage.getItem("sessionName")}
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end">
                   <li><Link className="dropdown-item" to="#">Meu Perfil</Link></li>
                   <li><hr className="dropdown-divider" /></li>
-                  <li><Link className="dropdown-item" to="/">Desconectar</Link></li>
+                  <li><button className="dropdown-item" onClick={Logout}>Desconectar</button></li>
                 </ul>
               </div>
             </li>
